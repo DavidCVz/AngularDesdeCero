918642abc6bc7d6d745cd49056c71e2a5e7bd707
DavidCVz <davidcarrillovz@outlook.com>
2023-05-04 13:39:01 -0600

    Deseño inicial

---
+++ 03-gifs-app/src/app/gifs/components/search-box/search-box/search-box.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+    <h5>Buscar:</h5>
+    <input type="text" placeholder="Buscar gifs..." id=""
+      class="form-control">
+  `
+})
+export class SearchBoxComponent {
+
+}
