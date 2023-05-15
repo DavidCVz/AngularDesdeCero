1b51b87762b41a9dc0f1859db8762f3acc076a0a
DavidCVz <davidcarrillovz@outlook.com>
2023-05-15 12:21:33 -0600

    Configuracion de las variables de entorno y json server
    -- Instalacion y configuracion del JSON server (carpeta Data)
    -- Configuracion de las variables de entorno
    (carpeta environments) y modificacion del archivo 'fileReplacements' en 'angular.json'

---
+++ 06-heroesApp/README.md
@@ -0,0 +1,7 @@
+# HeroesApp
+
+## Dev
+1. Clonar el proyecto
+2. Ejecutar el ```npm install```
+3. Levantar Backend ```npm run backend```
+4. Ejecutar la app ```npm start``` o bien el comando de ```ng serve```
---
+++ 06-heroesApp/src/app/heroes/heroes-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+
+const routes: Routes = [
+  // localhost:4200/heroes/
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      { path: 'new-hero', component: NewPageComponent },
+      { path: 'search', component: SearchPageComponent },
+      { path: 'edit/:id', component: NewPageComponent },
+      { path: 'list', component: ListPageComponent },
+       /** Cuando se defina una ruta donde se pase un parametro (path: ':id') a traves
+        * de la ruta padre, por conveniencia es mejor definirlo al final del conjunto
+        * de rutas, ya que si se pone al inicio o antes que la definicion de otras rutas
+        * hijas, dihos nombres de rutas serán considerados como los parametros
+        */
+      { path: ':id', component: HeroPageComponent },
+      { path: '**', redirectTo: 'list' }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HeroesRoutingModule { }
