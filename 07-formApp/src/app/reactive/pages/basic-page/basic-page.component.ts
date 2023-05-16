52c7c55161ba2563fa3f2b90cd1d6eb70b86143d
DavidCVz <davidcarrillovz@outlook.com>
2023-05-16 10:53:48 -0600

    Helpers Para Validaciones de formularios

--- 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// Caso donde el backend devuelve el siguiente objeto:
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 2500,
+  inStorage: 6
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit{
 
   /** NOTA: Para poder usar las directivas para los formularios html se debe
    * de importar el modulo 'ReactiveFormsModule' en los imports de cada
@@ -34,9 +41,64 @@ export class BasicPageComponent {
 
   constructor(private formBuilder: FormBuilder){}
 
+  ngOnInit(): void {
+    /* En un caso donde se quiera establecer los valores iniciales del
+    formulario a traves de una respuesta del back antes de cargar el
+    componente, se puede pasar esa respuesta al reset siempre y cuando los keys
+    del objeto que se receba coincidan con los keys del formulario*/
+    //this.myForm.reset(rtx5090);
+  }
+
+  /** Si luego de interactuar con el elemento, hay errores, devuelve 'true' */
+  isValidField(field: string): boolean | null {
+    //
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+  getFieldError( field: string): string | null{
+
+    /* Si no existe el elemento, devuelve null */
+    if ( !this.myForm.controls[field] ) { return null }
+
+    /** Si hay errores se almacenaran en la variable, sino quedará como un
+     * objeto vacio*/
+    const errors = this.myForm.controls[field].errors || {}
+
+    /** Se recorre el objeto con los errores, si un error cae en un caso
+     * entonce se devolverá el mensaje
+     */
+    for (const key of Object.keys(errors)){
+      switch( key ){
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Minimo ${ errors['minlength'].requiredLength} caracters`;
+      }
+    }
+    return null;
+  }
 
   // Obtener la informacion luego de disparar un Submit en el formulario
   onSave(): void{
+    if (this.myForm.invalid) {
+      /** En casos donde se desee disparar o ejecutar ciertas acciones o errores
+       * sobre el formulario cuando se trata de enviar un formulario en vacio por ejemplo,
+       * se puede utilizar los metodos de tipo 'markAll' del objeto de tipo 'FormGroup'
+      */
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
     console.log(this.myForm.value);
+
+    /* 'reset' reestablece los valores de los elementos del formulario a traves de
+    las keys que se pasen como argumentos del metodo, estos valores serán tomados
+    por el formulario, siempre y cuando hagan march con los nombres correctamente */
+    this.myForm.reset({
+      name: '',
+      price: 10,
+      inStorage: 50
+    });
   }
 }
